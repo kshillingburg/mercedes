8202cad20595347fd45935402a0272bf7287533d
kshilingburg <kshill1990@gmail.com>
2020-08-24 10:54:46 -0600

    fixed some of the selectors

--- pageObjects/mercedesObjects.js
+++ pageObjects/mercedesObjects.js
@@ -23,6 +23,7 @@ var mercedesCommands = {
 module.exports = {
     url: 'https://www.mbusa.com/en/vehicles/build/gle/coupe/gle53c4?category=exterior',
     elements: {
+        loadedPage: '#step-container',
         
         nextButton: 'button[class="button button_primary button--wide button--full-width-small-only mbs-build-steps__cta"]',
 
@@ -47,74 +48,74 @@ module.exports = {
             selector: '(//img[@alt="Select No-Image"])[2]',
             locateStrategy: 'xpath'},
         nightPackage: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[1]',
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[0]',
             locateStrategy: 'xpath'},
         runningBoards: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[2]',
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[1]',
             locateStrategy: 'xpath'},
         illuminatedStar: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[3]',
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[2]',
             locateStrategy: 'xpath'},
         chromeHandle: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[4]',
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[3]',
             locateStrategy: 'xpath'},
         
 
         entertainmentAndConvenience: 'button[id="entertainment"]',
         warmthAndComfort: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[1]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[0]',
+            locateStrategy: 'xpath'},
         airBalance: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[2]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[1]',
+            locateStrategy: 'xpath'},
         energizing: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[3]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[2]',
+            locateStrategy: 'xpath'},
         ventilatedSeats: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[4]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[3',
+            locateStrategy: 'xpath'},
         massageSeats: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[5]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[4]',
+            locateStrategy: 'xpath'},
         rapidHeatSeats: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[6]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[5]',
+            locateStrategy: 'xpath'},
         heated2ndRow: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[7]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[6]',
+            locateStrategy: 'xpath'},
         cupholders: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[8]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[7]',
+            locateStrategy: 'xpath'},
         climateControl: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[9]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[8]',
+            locateStrategy: 'xpath'},
         surroundSound: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[10]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[9]',
+            locateStrategy: 'xpath'},
         navigation: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[11]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[10]',
+            locateStrategy: 'xpath'},
         softDoors: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[12]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[11]',
+            locateStrategy: 'xpath'},
         interiorAssistant: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[13]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[12]',
+            locateStrategy: 'xpath'},
         heatedWindshield: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[14]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[13]',
+            locateStrategy: 'xpath'},
         headUpDisplay: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[15]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[14]',
+            locateStrategy: 'xpath'},
         bikeRack: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[16]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[15]',
+            locateStrategy: 'xpath'},
         rearEntertainment: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[17]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[16]',
+            locateStrategy: 'xpath'},
         luggageNet: {
-            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[18]',
-            locateStrategy: 'xpath', },
+            selector: '(//label[@class="form__checkbox-label form__checkbox-label--hidden"])[17]',
+            locateStrategy: 'xpath'},
 
 
     }
