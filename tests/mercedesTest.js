67e92d06b4ebf26459879fd23878497035960fa7
kshilingburg <kshill1990@gmail.com>
2020-08-24 16:17:40 -0600

    stashing

--- tests/mercedesTest.js
+++ tests/mercedesTest.js
@@ -61,44 +61,7 @@ module.exports = {
             .makeChanges('@checkBox8')
             .makeChanges('@checkBox9')
             .click('@summary')
-<<<<<<< HEAD
             .expect.element('@totalBuild').text.to.equal("Total Build $117,211.5")
-=======
-            .expect.element('@totalBuild').text.to.equal('Total Build $117,211.5')
-    },
-
-
-    'Quincy Dream Car': browser => {
-        mercedes
-            .makeChanges('@obsidianBlack')
-            .makeChanges('@wheels22inchBlack')
-            .makeChanges('@checkBox1')
-            .click('@nextButton')
-            .makeChanges('@blackNappaLeather')
-            .makeChanges('@designoFlamedAsh')
-            .makeChanges('@checkBox1')
-            .makeChanges('@checkBox6')
-            .click('@nextButton')
-            .makeChanges('@checkBox1')
-            .makeChanges('@checkBox4')
-            .makeChanges('@checkBox8')
-            .makeChanges('@checkBox14')
-            .makeChanges('@checkBox15')
-            .makeChanges('@checkBox17')
-            .makeChanges('@checkBox18')
-            .click('@nextButton')
-            .makeChanges('@checkBox1')
-            .makeChanges('@checkBox10')
-            .makeChanges('@nextButton')
-            .makeChanges('@checkBox3')
-            .makeChanges('@checkBox4')
-            .makeChanges('@checkBox5')
-            .makeChanges('@checkBox6')
-            .makeChanges('@checkBox7')
-            .makeChanges('@checkBox8')
-            .click('@nextButton')
-            .expect.element('@totalBuild').text.to.contain('Total Build $95,604')
->>>>>>> a2357300f528ca165542c2a597bbc906909ede85
     }
 
 }
