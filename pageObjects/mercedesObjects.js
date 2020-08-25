d726a2f49759b4b63a44811c18a280657145c2a8
Bri Davis <bridavis@Bris-iMac.local>
2020-08-25 11:11:04 -0500

    fixed runningTotalBuild selector to include text

--- pageObjects/mercedesObjects.js
+++ pageObjects/mercedesObjects.js
@@ -28,7 +28,7 @@ module.exports = {
         nextButton: 'button[class="button button_primary button--wide button--full-width-small-only mbs-build-steps__cta"]',
 
         runningTotalBuild: {
-            selector:'//span[@class="mbs-build-pricing__price "]',
+            selector://div[@class="mbs-build-pricing__price-container"]',
             locateStrategy: 'xpath'
         },
 
